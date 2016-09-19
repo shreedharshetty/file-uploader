21093030dbd9ca3aa31267876be7accf503d055e
Shreedharshetty <shreedhar888@gmail.com>
2016-09-19 13:18:15 +0530

    added bower and added steps to run this in readme.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# Blueimp Wistia Upload
+
+## Description
+
+[AngularJS](https://angularjs.org/) directive to upload videos to [Wistia](http://wistia.com/) using [Blueimp jQuery Uploader](https://github.com/blueimp/jQuery-File-Upload).
+
+## How to start
+`npm install -g bower` to install bower
+`bower install` to install the dependencies
+`python -m SimpleHTTPServer 7000` to start the simple http server
+open `http://localhost:7000/` in browser and start uploading files.
+
+##Note: Please generate your own Wistia password and pass it to directive as attribute
---
+++ assets/scripts/ngFileUploader.js
@@ -0,0 +1,70 @@
+(function() {
+  'use strict';
+  var directiveApp = angular.module('directiveApp', []);
+  directiveApp.directive('ngFileUpload', ['$timeout', '$sce', '$http', function($timeout, $sce, $http){
+    return {
+      restrict: 'E',
+      templateUrl: 'assets/templates/ng-upload-tmpl.html',
+      replace: true,
+      scope: {
+        id: "@",
+        wistiaPass: "@"
+      },
+      link: function(scope, element, attrs){
+        scope.hashId   = '';
+        scope.progress = 0;
+        scope.status   = 'idle';
+        scope.url      = '';
+
+        scope.checkStatus = function() {
+          var promise = $http({
+            method: 'GET',
+            url: 'https://api.wistia.com/v1/medias/' + scope.hashId + '.json?api_password=' + scope.wistiaPass
+          });
+          promise.then(function (response) {
+            scope.status = response.data.status || '';
+            //check if the status is ready, set url for iframe
+            if (scope.status == 'ready'){
+              scope.url = $sce.trustAsResourceUrl('http://fast.wistia.net/embed/iframe/' + scope.hashId);
+            } else if (scope.status != 'failed') {
+              //check status every 3s
+              $timeout(function(){
+                scope.checkStatus();
+              }, 3000);
+            }
+          });
+        };
+
+        $timeout(function(){
+          $('#uploaderInput').fileupload({
+            dataType: 'json',
+            formData: {
+              api_password: scope.wistiaPass
+            },
+            add: function (e, data) {
+              scope.hashId   = '';
+              scope.progress = 0;
+              scope.status   = 'uploading';
+              scope.url      = '';
+
+              data.submit();
+            },
+            done: function (e, data) {
+              if (data.result.hashed_id != '') {
+                scope.hashId = data.result.hashed_id;
+                scope.checkStatus();
+              }
+            },
+            progressall: function (e, data) {
+              if (data.total > 0) {
+                scope.$apply(function(){
+                  scope.progress = parseInt(data.loaded / data.total * 100, 10);
+                });
+              }
+            }
+          });
+        });
+      }
+    }
+  }])
+})();
